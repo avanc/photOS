0b745dc4ed0bf3b72b837a3bec7315410dfa88b4
Sven Klomp <mail@klomp.eu>
2020-11-14 21:44:25 +0100

    Fix package build

--- package/photoframe/photoframe.mk
+++ package/photoframe/photoframe.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-PHOTOFRAME_VERSION = 0.0.1
 PHOTOFRAME_LICENSE = GPL-3.0+
-#PHOTOFRAME_SITE = hello
 
 define PHOTOFRAME_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(LIBFOO_PKGDIR)/photoframe.sh $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(LIBFOO_PKGDIR)/S51photoframe $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -D -m 0700 $(LIBFOO_PKGDIR)/etc_photoframe $(TARGET_DIR)/etc
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/photoframe.sh $(TARGET_DIR)/bin
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S51photoframe $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/etc_photoframe/davfs2.conf $(TARGET_DIR)/etc
+	$(INSTALL) -D -m 0600 $(PHOTOFRAME_PKGDIR)/etc_photoframe/photoframe.conf $(TARGET_DIR)/etc
 endef
 
 
-$(eval $(autotools-package))
+$(eval $(generic-package))
