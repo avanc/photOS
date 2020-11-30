e216e75ebba785e523f3a588164299110e83c48a
Sven Klomp <mail@klomp.eu>
2020-11-30 08:18:17 +0100

    Install files into subdirectory

--- package/photoframe/photoframe.mk
+++ package/photoframe/photoframe.mk
@@ -8,9 +8,9 @@ PHOTOFRAME_LICENSE = GPL-3.0+
 
 define PHOTOFRAME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/photoframe.sh $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S51photoframe $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/etc_photoframe/davfs2.conf $(TARGET_DIR)/etc
-	$(INSTALL) -D -m 0600 $(PHOTOFRAME_PKGDIR)/etc_photoframe/photoframe.conf $(TARGET_DIR)/etc
+	$(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/S80photoframe $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/etc_photoframe/davfs2.conf $(TARGET_DIR)/etc/photoframe
+	$(INSTALL) -D -m 0600 $(PHOTOFRAME_PKGDIR)/etc_photoframe/photoframe.conf $(TARGET_DIR)/etc/photoframe
 endef
 
 
