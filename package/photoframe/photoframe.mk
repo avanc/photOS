257522747eb169e3f894a9f20b6b606115e4d2ff
Sven Klomp <mail@klomp.eu>
2021-05-12 15:12:47 +0200

    Fix wrong configuration path

--- package/photoframe/photoframe.mk
+++ package/photoframe/photoframe.mk
@@ -13,7 +13,7 @@ define PHOTOFRAME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/etc_photoframe/davfs2.conf $(TARGET_DIR)/etc/photoframe/davfs2.conf
 	$(INSTALL) -D -m 0644 $(PHOTOFRAME_PKGDIR)/images/noimages.png $(TARGET_DIR)/usr/share/photoframe/noimages.png
 
-    $(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/pir/motion_detector.py $(TARGET_DIR)/usr/bin/motion_detector.sh
+    $(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/pir/motion_detector.py $(TARGET_DIR)/usr/bin/motion_detector.py
     $(INSTALL) -D -m 0755 $(PHOTOFRAME_PKGDIR)/pir/S81motiondetector $(TARGET_DIR)/etc/init.d/S81motiondetector
 endef
 
