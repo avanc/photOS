d8c7ed3aac13c9f94a26047bcf7f54712d9ffe2d
dirk <info@universalappfactory.de>
2021-12-04 10:28:58 +0100

    - added missing packages to Config.in - renamed matrix_config.yml to matrix_config_example.yml

--- package/photoframe-matrix/photoframe-matrix.mk
+++ package/photoframe-matrix/photoframe-matrix.mk
@@ -6,7 +6,7 @@
 
 define PHOTOFRAME_MATRIX_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(PHOTOFRAME_MATRIX_PKGDIR)/S82matrixclient $(TARGET_DIR)/etc/init.d/S82matrixclient
-	$(INSTALL) -D -m 644 $(PHOTOFRAME_MATRIX_PKGDIR)/matrix_config.yml $(TARGET_DIR)/data/photoframe/conf/matrix_config.yml
+	$(INSTALL) -D -m 644 $(PHOTOFRAME_MATRIX_PKGDIR)/matrix_config_example.yml $(TARGET_DIR)/etc/photoframe/matrix_config_example.yml
 endef
 
 $(eval $(generic-package))
