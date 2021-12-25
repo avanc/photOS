24682d885d1460a6ecebbdfba09a4df95ec9ce51
dirk <info@universalappfactory.de>
2021-12-25 10:30:48 +0100

    updateded version

--- package/python-matrix-photos/python-matrix-photos.mk
+++ package/python-matrix-photos/python-matrix-photos.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MATRIX_PHOTOS_VERSION = 0.0.1b1
+PYTHON_MATRIX_PHOTOS_VERSION = 0.0.2
 PYTHON_MATRIX_PHOTOS_SOURCE = matrix-photos-$(PYTHON_MATRIX_PHOTOS_VERSION).tar.gz
-PYTHON_MATRIX_PHOTOS_SITE = https://files.pythonhosted.org/packages/a1/0d/1feda8bef04b537b24b9e0cc8491d77c92210ba05ed60f80b98060a9dec5
+PYTHON_MATRIX_PHOTOS_SITE = https://files.pythonhosted.org/packages/b2/70/267b57ee783cbbc4f268ad9ac208140459b5d97ddec736c6ed1b928f4ab5
 
 PYTHON_MATRIX_PHOTOS_SETUP_TYPE = setuptools
 
